a4236c60519883258412b7395a59fdf1cc597196
OLia <celia.ouedrao@gmail.com>
2024-11-08 15:58:56 +0100

    Modification README, models et main

---
+++ src/main.py
@@ -0,0 +1,27 @@
+from fastapi import FastAPI
+from src.database import Base, engine
+from src.models.models import Client, Commande, Detail
+
+app = FastAPI
+
+Base.metadata.create_all(engine)
+
+
+@app.get("/")
+def home():
+    return {}
+
+
+@app.get("/client/")
+def clients():
+    return {}
+
+
+@app.get("/client/commande/")
+def adresses_clients():
+    return {}
+
+
+@app.get("/client/commande/detail/")
+def communes_clients():
+    return {}
