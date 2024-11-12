b56e0a50f03b2bf11e4e78d63029890108f32f71
OLia <celia.ouedrao@gmail.com>
2024-11-12 16:02:20 +0100

    doesn't work yet

--- src/main.py
+++ src/main.py
@@ -14,16 +14,16 @@ def home():
     return {"API démarrée"}
 
 
-@app.get("/client/")
-def clients():
-    return {"Accès à la table Client"}
+# @app.get("/client/")
+# def clients():
+#     return {"Accès à la table Client"}
 
 
-@app.get("/client/commande/")
-def commandes_clients():
-    return {"Accès à la table Commande"}
+# @app.get("/client/commande/")
+# def commandes_clients():
+#     return {"Accès à la table Commande"}
 
 
-@app.get("/client/commande/detail/")
-def detail_commandes_clients():
-    return {"Accès à la table Détail_Commande"}
+# @app.get("/client/commande/detail/")
+# def detail_commandes_clients():
+#     return {"Accès à la table Détail_Commande"}
