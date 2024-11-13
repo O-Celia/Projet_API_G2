9cec2c64dbb0fa7ed860c75e47da282478194ef0
Yakine-2 <yakine.zardi@gmail.com>
2024-11-13 16:58:05 +0100

    Update commande_controller.py

--- src/controller/commande_controller.py
+++ src/controller/commande_controller.py
@@ -1,6 +1,7 @@
 from sqlalchemy.orm import Session
 from src.models.model import Commande
-from src.schema.commande_schema import CommandeCreateSchema
+from src.schema.commande_schema import CommandeCreateSchema, CommandeUpdateSchema
+from fastapi import HTTPException
 
 def get_commandes(db: Session):
     return db.query(Commande).all()
@@ -12,8 +13,24 @@ def create_commande(commande: CommandeCreateSchema, db: Session):
     db.refresh(db_commande)
     return db_commande
 
-def update_commande():
-    pass
+def update_commande(codcde: int, commande: CommandeUpdateSchema, db: Session):
+    maj_commande= db.query(Commande).filter(Commande.codcde == codcde).first()
+    if not maj_commande:
+        raise HTTPException(status_code=404, detail="commande non trouvé")
+    
+    for key, value in commande.model_dump(exclude_unset=True).items():
+        setattr(maj_commande, key, value)
 
-def delete_commande():
-    pass
+    db.commit()
+    db.refresh(maj_commande)
+    return maj_commande
+
+
+def delete_commande(codcde: int, db: Session):
+    del_commande = db.query(Commande).filter(Commande.codcde == codcde).first()
+    if not del_commande:
+        raise HTTPException(status_code=404, detail="Commande non trouvé")
+    
+    db.delete(del_commande)
+    db.commit()
+    return del_commande
