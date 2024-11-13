d5af00acf953b7b1510cc59479a5385e477351b1
Yakine-2 <yakine.zardi@gmail.com>
2024-11-13 16:57:35 +0100

    Update detail_commande_controller.py

--- src/controller/detail_commande_controller.py
+++ src/controller/detail_commande_controller.py
@@ -1,6 +1,7 @@
 from sqlalchemy.orm import Session
 from src.models.model import Detail
-from src.schema.detail_schema import DetailCreateSchema
+from src.schema.detail_schema import DetailCreateSchema, DetailUpdateSchema
+from fastapi import HTTPException
 
 def get_details(db: Session):
     return db.query(Detail).all()
@@ -12,8 +13,24 @@ def create_detail(detail: DetailCreateSchema, db: Session):
         db.refresh(new_detail)
         return new_detail
 
-def update_detail():
-    pass
+def update_detail(id: int, detail: DetailUpdateSchema, db: Session):
+    maj_detail= db.query(Detail).filter(Detail.id == id).first()
+    if not maj_detail:
+        raise HTTPException(status_code=404, detail="detail non trouvé")
+    
+    for key, value in detail.model_dump(exclude_unset=True).items():
+        setattr(maj_detail, key, value)
 
-def delete_detail():
-    pass
+    db.commit()
+    db.refresh(maj_detail)
+    return maj_detail
+
+
+def delete_detail(id: int, db: Session):
+    del_detail = db.query(Detail).filter(Detail.id == id).first()
+    if not del_detail:
+        raise HTTPException(status_code=404, detail="detail non trouvé")
+    
+    db.delete(del_detail)
+    db.commit()
+    return del_detail
