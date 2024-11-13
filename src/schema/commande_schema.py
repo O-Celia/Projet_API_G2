feb29812751e80cd44cd04b08c6269f0551d77a5
Yakine-2 <yakine.zardi@gmail.com>
2024-11-13 17:00:36 +0100

    Update commande_schema.py

--- src/schema/commande_schema.py
+++ src/schema/commande_schema.py
@@ -1,24 +1,29 @@
 from pydantic import BaseModel
 from datetime import date
+from typing import Optional
 
 class CommandeBaseSchema(BaseModel):
-    codcli: int
-    codcde : int 
-    nbcolis : int
-    barchive : int
-    bstock : int
-    timbrecli : float
-    timbrecde : float
-    cheqcli : float
-    datcde : date
     
+    codcli: int
+    datcde : date
+    nbcolis : Optional[int] = None
+    barchive : Optional[int] = None
+    bstock : Optional[int] = None
+    timbrecli : Optional[int] = None
+    timbrecde : Optional[int] = None
+    cheqcli : Optional[int] = None
+    cdeComt: Optional[int]= None
 
 
 class CommandeCreateSchema(CommandeBaseSchema):
+    
+    pass
+
+class CommandeUpdateSchema(CommandeBaseSchema):
     pass
     
 class CommandeInDBSchema(CommandeBaseSchema):
-    cdeComt: str
+    codcde : int 
    
     class Config():
         from_attributes = True
