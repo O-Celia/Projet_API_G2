bb31e07f39ffe03c4fd0de6541bb07f02caf3303
Yakine-2 <yakine.zardi@gmail.com>
2024-11-13 12:45:32 +0100

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,26 +1,39 @@
 from fastapi import FastAPI
+# Chemin vers le dossier racine contenant `src`
 from src.database import Base, engine
-from src.router.client_router import router_client
+from src.router.router_client import router_client
+from src.models.model import Client, Commande, Detail
+from src.router.router_commandes import router_commande
+from src.router.router_detail import router_detail
+app = FastAPI()
+
+Base.metadata.create_all(engine)
 
 app = FastAPI(
     title="Intranet DIGICHEESE",
     description="Nouvelle API Digicheese",
     version="0.0.1"
 )
-# Ajouter le routeur du client
-app.include_router(router_client)
 
-Base.metadata.create_all(engine)
+app.include_router(router_client)
+app.include_router(router_commande)
+app.include_router(router_detail)
 
 @app.get("/")
 def home():
     return {"API démarrée"}
 
-@app.get("/client/commande/")
-def adresses_clients():
-    return {"Accès à la table Commande"}
+
+# @app.get("/client/")
+# def clients():
+#     return {"Accès à la table Client"}
 
 
-@app.get("/client/commande/detail/")
-def communes_clients():
-    return {"Accès à la table Détail_Commande"}
+# @app.get("/commande/")
+# def adresses_clients():
+#     return {"Accès à la table Commande"}
+
+
+# @app.get("/detail/")
+# def communes_clients():
+#     return {"Accès à la table Détail_Commande"}
